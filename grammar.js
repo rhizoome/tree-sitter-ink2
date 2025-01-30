0f3c5f12d978c5d1990d566d485d381c26b4718e
Rhizoome <alter@rhizoome.ch>
2025-01-30 03:47:34 +0100

    basic rules

--- grammar.js
+++ grammar.js
@@ -1,10 +1,62 @@
+const CATCH_ALL = -2;
+
 module.exports = grammar({
-  name: "ink",
+    name: "ink",
+    extras: $ => [],
+    conflicts: $ => [
+        [$.knot],
+        [$.body],
+    ],
 
-  rules: {
+    rules: {
 
-    program: $ => repeat(/\w/)
+        program: $ => seq(
+            repeat($.knot),
+            /\n/
+        ),
 
-  }
+        knot: $ => seq(
+            $.knot_start,
+            field("name", $.identifier),
+            optional($.rest),
+            optional($.body)
+        ),
+
+        body: $ => repeat1(
+            choice(
+                $.stitch,
+                $.dialog
+            )
+        ),
+
+
+        stitch: $ => seq(
+            $.stitch_start,
+            field("name", $.identifier),
+            optional($.rest)
+        ),
+
+        inline_logic: $ => seq(
+            "{",
+            /[^}]+/,
+            "}"
+        ),
+
+        dialog: $ => seq(
+            /\n/,
+            repeat1(
+                choice(
+                    field("text", $.text),
+                    $.inline_logic
+                )
+            )
+        ),
+
+        identifier: $ => /\p{L}+/,
+        knot_start: $ => /\n\s*={2,}=*\s*/,
+        stitch_start: $ => /\n\s*=\s*/,
+        text: $ => /[^\n{]+/,
+        rest: $ => /[^\n]+/,
+    }
 
 })
