44fc9c44191127b5e948aed911dfa7664fa8a78b
Rhizoome <alter@rhizoome.ch>
2025-01-31 20:39:29 +0100

    basic conflict free parsing

--- grammar.js
+++ grammar.js
@@ -1,8 +1,9 @@
 module.exports = grammar({
     name: "ink",
+    extras: $ => [],
     conflicts: $ => [
-        [$.body],
-        [$.vocabular]
+        //[$.body],
+        //[$.vocabular]
     ],
     externals: $ => [
         $.arrow,
@@ -10,121 +11,28 @@ module.exports = grammar({
         $.body_start,
         $.knot_start,
         $.choice_start,
-        $.empty_line,
         $.line_end
     ],
 
     rules: {
 
         program: $ => repeat(
-            choice(
-                $.body,
-                //$.knot,
-                $.empty_line
+            seq(
+                choice(
+                    $.body_start,
+                    $.knot_start,
+                    $.choice_start
+                ),
+                repeat(choice(
+                    $.arrow,
+                    $.minus,
+                    $.other
+                )),
+                $.line_end
             )
         ),
 
-        knot: $ => seq(
-            $.knot_start,
-            /\s*===*\s/,
-            $.identifier
-        ),
+        other: $ => prec(-1, /./),
 
-        body: $ => repeat1(
-            choice(
-                $.paragraph,
-                //$.dialog_line,
-            )
-        ),
-
-        paragraph: $ => seq(
-            $.body_start,
-            $.text_line,
-            $.line_end
-        ),
-
-        dialog_line: $ => seq(
-            $.choice_start,
-            /\s*\*/,
-            $.text_line,
-        ),
-
-        text_line: $ => repeat1(
-            choice(
-                $.arrow,
-                $.vocabular
-            )
-        ),
-
-        vocabular: $ => repeat1(
-            choice(
-                /[\p{L}_]+/,
-                $.minus, "-"
-            )
-        ),
-
-        identifier: $ => /[\p{L}_]+/,
-
-        //program: $ => seq(
-        //    optional($.body),
-        //    repeat($.knot),
-        //    repeat(/\n/)
-        //),
-
-        //knot: $ => seq(
-        //    $.knot_start,
-        //    field("name", $.identifier),
-        //    optional($.rest),
-        //    optional($.body)
-        //),
-
-        //body: $ => repeat1(
-        //    choice(
-        //        $.stitch,
-        //        $.dialog
-        //    )
-        //),
-
-        //stitch: $ => seq(
-        //    $.stitch_start,
-        //    field("name", $.identifier),
-        //    optional($.rest)
-        //),
-
-        //inline_logic: $ => seq(
-        //    "{",
-        //    /[^}]+/,
-        //    "}"
-        //),
-
-        //dialog: $ => seq(
-        //    /\n/,
-        //    repeat1(
-        //        choice(
-        //            /\n/,
-        //            $.inline_logic,
-        //            field("text", repeat1(
-        //                choice(
-        //                    "-",
-        //                    $.text
-        //                )
-        //            )),
-        //        )
-        //    ),
-        //    optional($.divert)
-        //),
-
-        //divert: $ => seq(
-        //    $.divert_start,
-        //    $.identifier
-        //),
-
-        //divert_start: $ => /->\s*/,
-        //identifier: $ => /\p{L}+/,
-        //knot_start: $ => /\n\s*={2,}=*\s*/,
-        //stitch_start: $ => /\n\s*=\s*/,
-        //text: $ => /[^\n{-]+/,
-        //rest: $ => /[^\n]+/,
     }
-
 })
